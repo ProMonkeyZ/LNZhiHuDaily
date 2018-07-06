634f0398ed4a58ac1d9d9cd8a8af187e14f76693
张立宁 <zhanglining@syswin.com>
2018-07-06 11:31:53 +0800

    添加初始文件

---
+++ LNZhiHuDaily/LNZhiHuDaily/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LNZhiHuDaily
+//
+//  Created by 张立宁 on 2018/7/6.
+//  Copyright © 2018年 ZLN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LNZhiHuDaily/LNZhiHuDaily/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LNZhiHuDaily
+//
+//  Created by 张立宁 on 2018/7/6.
+//  Copyright © 2018年 ZLN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
